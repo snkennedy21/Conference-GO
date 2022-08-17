96ffc4af2cc9c66403fc7c7a26d397fd6489d5f0
Sean Kennedy <snkennedy21@gmail.com>
2022-08-17 16:36:14 -0400

    finish exploration

--- attendees/api_views.py
+++ attendees/api_views.py
@@ -2,7 +2,9 @@ from django.http import JsonResponse
 from common.json import ModelEncoder
 from .models import Attendee
 from events.api_views import ConferenceListEncoder
-
+from django.views.decorators.http import require_http_methods
+import json
+from events.models import Conference
 
 
 #######################
@@ -14,12 +16,32 @@ class AttendeeListEncoder(ModelEncoder):
         "name"
     ]
 
+@require_http_methods(["GET", "POST"])
 def api_list_attendees(request, conference_id):
-    attendees = Attendee.objects.filter(conference=conference_id)
-    return JsonResponse(
-        {"attendees": attendees},
-        encoder=AttendeeListEncoder,
-    )
+    if request.method == "GET":
+        attendees = Attendee.objects.filter(conference=conference_id)
+        return JsonResponse(
+            {"attendees": attendees},
+            encoder=AttendeeListEncoder,
+        )
+    else:
+        content = json.loads(request.body)
+        try:
+            conference = Conference.objects.get(id=conference_id)
+            content["conference"] = conference
+        except Conference.DoesNotExist:
+            return JsonResponse(
+                {"message": "Invalid conference id"},
+                status=400,
+            )
+
+        attendee = Attendee.objects.create(**content)
+        return JsonResponse(
+            attendee,
+            encoder=AttendeeDetailEncoder,
+            safe=False,
+        )
+
 
 
 #########################
