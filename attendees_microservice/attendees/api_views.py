98973fafb56f3eed70b0daafa032bcbab09804a3
Sean Kennedy <snkennedy21@gmail.com>
2022-08-22 16:53:23 -0400

    break application into two separate microservices. Attendees and main

--- attendees_microservice/attendees/api_views.py
+++ attendees_microservice/attendees/api_views.py
@@ -1,15 +1,15 @@
 from django.http import JsonResponse
 from common.json import ModelEncoder
-from .models import Attendee
-from events.api_views import ConferenceListEncoder
+from .models import Attendee, ConferenceVO
 from django.views.decorators.http import require_http_methods
 import json
-from events.models import Conference
 
 
-#######################
-# Show Attendees List #
-#######################
+class ConferenceVODetailEncoder(ModelEncoder):
+    model = ConferenceVO
+    properties = ["name", "import_href"]
+
+
 class AttendeeListEncoder(ModelEncoder):
     model = Attendee
     properties = [
@@ -17,9 +17,9 @@ class AttendeeListEncoder(ModelEncoder):
     ]
 
 @require_http_methods(["GET", "POST"])
-def api_list_attendees(request, conference_id):
+def api_list_attendees(request, conference_vo_id=None):
     if request.method == "GET":
-        attendees = Attendee.objects.filter(conference=conference_id)
+        attendees = Attendee.objects.filter(conference=conference_vo_id)
         return JsonResponse(
             {"attendees": attendees},
             encoder=AttendeeListEncoder,
@@ -27,9 +27,10 @@ def api_list_attendees(request, conference_id):
     else:
         content = json.loads(request.body)
         try:
-            conference = Conference.objects.get(id=conference_id)
+            conference_href = f'/api/conferences/{conference_vo_id}/'
+            conference = ConferenceVO.objects.get(import_href=conference_href)
             content["conference"] = conference
-        except Conference.DoesNotExist:
+        except ConferenceVO.DoesNotExist:
             return JsonResponse(
                 {"message": "Invalid conference id"},
                 status=400,
@@ -43,10 +44,6 @@ def api_list_attendees(request, conference_id):
         )
 
 
-
-#########################
-# Show Attendee Details #
-#########################
 class AttendeeDetailEncoder(ModelEncoder):
     model = Attendee
     properties = [
@@ -57,7 +54,7 @@ class AttendeeDetailEncoder(ModelEncoder):
         "conference",
     ]
     encoders = {
-        "conference": ConferenceListEncoder(),
+        "conference": ConferenceVODetailEncoder(),
     }
 
 @require_http_methods(["GET", "PUT", "DELETE"])
--- attendees_microservice/attendees/models.py
+++ attendees_microservice/attendees/models.py
@@ -1,7 +1,12 @@
+from xdrlib import ConversionError
 from django.db import models
 from django.urls import reverse
 from django.core.exceptions import ObjectDoesNotExist
 
+
+class ConferenceVO(models.Model):
+    import_href = models.CharField(max_length=200, unique=True)
+    name = models.CharField(max_length=200)
 
 class Attendee(models.Model):
     """
@@ -15,7 +20,7 @@ class Attendee(models.Model):
     created = models.DateTimeField(auto_now_add=True)
 
     conference = models.ForeignKey(
-        "events.Conference",
+        ConferenceVO,
         related_name="attendees",
         on_delete=models.CASCADE,
     )
