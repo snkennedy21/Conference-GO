b59d66389527d02aba49246468e4d5d55dd2b3a6
Sean Kennedy <snkennedy21@gmail.com>
2022-08-22 17:01:45 -0400

    create a network for the monolith app and the attendees microservice

--- attendees_microservice/attendees_bc/settings.py
+++ attendees_microservice/attendees_bc/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "django_crontab",
     "attendees.apps.AttendeesConfig",
     'django.contrib.admin',
     'django.contrib.auth',
@@ -121,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRONJOBS = [
+    ("* * * * *", "attendees.poll.get_conferences"),
+]
